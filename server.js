5a869764673942d41694b784ecbffb2884c69f08
Gaurav Verma <tussharverma94@gmail.com>
2021-05-15 12:28:16 +0530

    added set to views

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 const main_route = require("./Routes/locate")
+const path = require('path')
+
 // const adult = require("./Routes/adult")
 const SERVER_PORT = process.env.PORT || 4444
 
 app.set("view engine", "hbs")
+app.set('views', path.join(__dirname, 'views')); 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
